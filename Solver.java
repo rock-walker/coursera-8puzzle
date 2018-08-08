f7ee5ffd24f50437b9ee72523b383a6b16e13efd
Andrei Melnikau <melnikov.work@gmail.com>
2018-08-09 00:37:06 +0300

    puzzle00 - puzzle10 - completed. puzzle11 = 22

--- Solver.java
+++ Solver.java
@@ -1,77 +1,159 @@
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-import java.util.ArrayList;
-import java.util.Comparator;
+import edu.princeton.cs.algs4.Stack;
+
 import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.ArrayList;
+
 
 public class Solver {
-    
+
     private boolean isSolvable = true;
     private int moves = 0;
-    private List<Board> solution = new ArrayList<Board>();
+    public Board initialBoard;
+    private Stack<SearchNode> solution = new Stack<SearchNode>();
 
-    private Comparator<Board> boardComparator = new Comparator<Board>() {
+    private Comparator<SearchNode> boardComparator = new Comparator<SearchNode>() {
 
-        public int compare(Board b1, Board b2) {
-            int priority1 = b1.manhattan();
-            int priority2 = b2.manhattan();
-            if (priority1 == priority2) {
-                return 0;
-            }
-            else if (priority1 < priority2) {
-                return -1;
-            }
-
-            return 1;
+        public int compare(SearchNode b1, SearchNode b2) {
+            int priority1 = b1.priority + b1.move;
+            int priority2 = b2.priority + b2.move;
+            return (priority1 < priority2) ? -1 : 1;
         }
     };
+
+    private class SearchNode {
+        SearchNode(int priority, int move, Board board) {
+            this.priority = priority;
+            this.move = move;
+            this.board = board;
+        }
+        public int priority;
+        public int move;
+        public Board board;
+    }
 
     public Solver(Board initial) {
         if (initial == null){
             throw new java.lang.IllegalArgumentException();
         }
-        Board current = initial;
-        Board previous = null;
-        int shortPath;
+        initialBoard = initial;
+        SearchNode snInitial = new SearchNode(0, moves, initial);
 
-        MinPQ<Board> queue = new MinPQ<Board>(boardComparator);
-        queue.insert(initial);
-        Board newCurrentBoard = null;
-        
-        while (!current.isGoal() && moves < 11) {
+        SearchNode current = snInitial;
+        SearchNode previous = current;
+
+        int shortPath;
+        int prevShortPath = Integer.MAX_VALUE;
+
+        MinPQ<SearchNode> queue = new MinPQ<SearchNode>(boardComparator);
+        //queue.insert(snInitial);
+        solution.push(snInitial);
+
+        while (!current.board.isGoal()){
+            current = findNext(current, previous, queue, Integer.MAX_VALUE);
+        }
+/*
+        while (!current.board.isGoal()) {
             moves++;
-            solution.add(queue.delMin());
+            // SearchNode minPriorSn = queue.delMin();
+            // solution.push(minPriorSn);
+
             shortPath = Integer.MAX_VALUE;
-            newCurrentBoard = null;
-            
-            for (Board b : current.neighbors()) {
-                if (b.equals(previous)) {
+
+            for (Board b : current.board.neighbors()) {
+                if (b.equals(previous.board) || b.equals(initial)) {
                     continue;
                 }
-                
-                queue.insert(b);
-                int c = maxPriority(shortPath, b);
+                int priority = b.manhattan();
+                SearchNode sn = new SearchNode(priority, moves, b);
+                queue.insert(sn);
+
+                int c = maxPriority(shortPath, priority);
                 if (c > 0) {
                     shortPath = c;
-                    newCurrentBoard = b;
                 }
             }
+
             previous = current;
-            current = newCurrentBoard;
-            StdOut.println(current);
+            current = queue.delMin();
+
+            if (current.move != moves) {
+                shortPath = Integer.MAX_VALUE;
+            }
+
+
+            if (prevShortPath < shortPath) {
+
+                while (!queue.isEmpty() && queue.min().move != 1) {
+                   queue.delMin();
+                }
+
+                current = queue.delMin();
+                moves = 1;
+
+                while (!queue.isEmpty() && queue.min().move != 1) {
+                    queue.delMin();
+                }
+
+                while (solution.size() != moves) {
+                    solution.pop();
+                }
+
+                prevShortPath = Integer.MAX_VALUE; // current.move + current.priority;
+                StdOut.println("wrong path");
+            } else {
+                prevShortPath = shortPath;
+                StdOut.println(current.board);
+            }
+
+            solution.push(current);
         }
-        
-        if (current.isGoal()) {
-            solution.add(newCurrentBoard);
-        }
+*/
     } // find a solution to the initial board (using the A* algorithm)
-    
-    private int maxPriority(int currMax, Board b) {
-        int prior = b.manhattan() + moves;
+
+    private SearchNode findNext(SearchNode current, SearchNode previous, MinPQ<SearchNode> queue, int shortPath) {
+        moves++;
+        for (Board b : current.board.neighbors()) {
+            if (b.equals(previous.board) || b.equals(initialBoard)) {
+                continue;
+            }
+            int priority = b.manhattan();
+            SearchNode sn = new SearchNode(priority, moves, b);
+            queue.insert(sn);
+
+            int c = maxPriority(shortPath, priority);
+            if (c > 0) {
+                shortPath = c;
+            }
+        }
+
+        previous = current;
+        current = queue.delMin();
+
+        if (current.move != moves) {
+            shortPath = Integer.MAX_VALUE;
+            solution.pop();
+            moves--;
+        }
+
+        solution.push(current);
+
+        if (!current.board.isGoal()) {
+            return findNext(current, previous, queue, shortPath);
+        }
+        else {
+            return current;
+        }
+    }
+
+    private int maxPriority(int currMax, int priority) {
+        int prior = priority + moves;
         if (prior <= currMax) {
-            currMax = prior;
-            return currMax;
+            return prior;
         }
         return 0;
     }
@@ -85,7 +167,14 @@ public class Solver {
     } // min number of moves to solve initial board; -1 if unsolvable
     
     public Iterable<Board> solution() {
-        return solution;
+        List<Board> list = new ArrayList<Board>();
+        while(!solution.isEmpty()) {
+            list.add(solution.pop().board);
+        }
+        // List<Board> shallowCopy = list.subList(0, list.size());
+        Collections.reverse(list);
+
+        return list;
     } // sequence of boards in a shortest solution; null if unsolvable
     
     public static void main(String[] args) {
